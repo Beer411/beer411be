07a3b4812de3581701668241bf4049bbb5fd17bd
Mike Vaughan <dvaugha1@gmail.com>
2015-03-19 13:04:41 -0400

    Mailer should work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,15 @@ class UsersController < ApplicationController
 
   def bug_report
 
+
     render :bug_report
 
     # redirect_to beers_list_path
   end
 
   def submit
-    BugMailer.bugs(params[:message]).deliver_now
+    @message = params[:bug_report][:message]
+    BugMailer.bugs(@message).deliver_now
     render json: { message: 'Message Sent' }
   end
 end
